acbdbb2d17f4bff7e85d2ce6700eca15a4e06fbb
belyashnikovatn <101242667+belyashnikovatn@users.noreply.github.com>
2024-05-01 21:34:01 +0500

    add modelForms

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -1,20 +1,35 @@
 from django import forms
-from birthday.models import Birthday
+from birthday.models import Birthday, Contest
 
 
 class BirthdayForm(forms.ModelForm):
-    first_name = forms.CharField(label='Имя', max_length=20)
-    last_name = forms.CharField(label='Фамилия', required=False,
-                                help_text='Необязательное поле')
-    birthday = forms.DateField(label='Дата рождения',
-                               widget=forms.DateInput(attrs={'type': 'date'}))
+
+    class Meta():
+        model = Birthday
+        fields = '__all__'
+        widgets = {
+            'birthday': forms.DateInput(attrs={'type': 'date'})
+        }
+    # first_name = models.CharField('Имя', max_length=20)
+    # last_name = models.CharField('Фамилия', blank=True,
+    #                              help_text='Необязательное поле',
+    #                              max_length=20)
+    # birthday = models.DateField('Дата рождения')
 
 
-class ContestForm(forms.Form):
-    title = forms.CharField(label='Название', max_length=20)
-    description = forms.CharField(label='Описание',
-                                  widget=forms.Textarea(attrs={'rows': '5'}))
-    price = forms.IntegerField(min_value=10, max_value=100, label='Цена',
-                               help_text='Рекомендованная розничная цена')
-    comment = forms.CharField(required=False, label='Комментарий',
-                              widget=forms.Textarea(attrs={'rows': '3'}))
+class ContestForm(forms.ModelForm):
+
+    class Meta:
+        model = Contest
+        fields = '__all__'
+        widgets = {
+            'description': forms.Textarea(attrs={'cols': '22', 'rows': '5'}),
+            'comment': forms.Textarea(attrs={'cols': '22', 'rows': '5'})
+        }
+    # title = forms.CharField(label='Название', max_length=20)
+    # description = forms.CharField(label='Описание',
+    #                               widget=forms.Textarea(attrs={'rows': '5'}))
+    # price = forms.IntegerField(min_value=10, max_value=100, label='Цена',
+    #                            help_text='Рекомендованная розничная цена')
+    # comment = forms.CharField(required=False, label='Комментарий',
+    #                           widget=forms.Textarea(attrs={'rows': '3'}))
--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Birthday(models.Model):
@@ -7,3 +8,15 @@ class Birthday(models.Model):
                                  help_text='Необязательное поле',
                                  max_length=20)
     birthday = models.DateField('Дата рождения')
+
+
+class Contest(models.Model):
+    title = models.CharField('Название', max_length=20)
+    description = models.TextField('Описание')
+    price = models.IntegerField('Цена',
+                                validators=[
+                                    MinValueValidator(10),
+                                    MaxValueValidator(100)
+                                    ],
+                                help_text='Рекомендованная розничная цена')
+    comment = models.TextField('Комментарий', blank=True)
