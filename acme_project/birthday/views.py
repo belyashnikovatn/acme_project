a6feecf37485104805c033125c38a3113d5fd8fb
Tania Belyashnikova <belyashnikova.tn@gmail.com>
2024-05-02 16:09:35 +0500

    add class Views

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,7 +1,39 @@
 from django.shortcuts import get_object_or_404, redirect, render
+from django.core.paginator import Paginator
+from django.views.generic import CreateView, DeleteView, ListView, UpdateView
+from django.urls import reverse_lazy
+
 from birthday.forms import BirthdayForm, ContestForm
 from birthday.utils import calculate_birthday_countdown
 from birthday.models import Birthday
+
+
+class BirthdayListView(ListView):
+    model = Birthday
+    ordering = 'id'
+    paginate_by = 3
+
+
+class BirthdayMixin:
+    model = Birthday
+    success_url = reverse_lazy('birthday:list')
+
+
+class BirhdayFormMixin:
+    form_class = BirthdayForm
+    template_name = 'birthday/birthday.html'
+
+
+class BirthdayCreateView(BirthdayMixin, BirhdayFormMixin,  CreateView):
+    pass
+
+
+class BirthdayUpdateView(BirthdayMixin, BirhdayFormMixin, UpdateView):
+    pass
+
+
+class BirthdayDelete(BirthdayMixin, DeleteView):
+    pass
 
 
 def birthday(request, pk=None):
@@ -12,7 +44,7 @@ def birthday(request, pk=None):
     form = BirthdayForm(request.POST or None,
                         files=request.FILES or None,
                         instance=instance)
-    
+
     context = {'form': form}
     if form.is_valid():
         form.save()
@@ -23,10 +55,14 @@ def birthday(request, pk=None):
     return render(request, 'birthday/birthday.html', context=context)
 
 
-def birthday_list(request):
-    birthdays = Birthday.objects.all().order_by('first_name')
-    context = {'birthdays': birthdays}
-    return render(request, 'birthday/birthday_list.html', context=context)
+# def birthday_list(request):
+#     birthdays = Birthday.objects.all().order_by('first_name')
+#     paginator = Paginator(birthdays, 3)
+#     page_number = request.GET.get('page')
+#     page_obj = paginator.get_page(page_number)
+#     context = {'page_obj': page_obj}
+
+#     return render(request, 'birthday/birthday_list.html', context=context)
 
 
 def delete_birthday(request, pk):
